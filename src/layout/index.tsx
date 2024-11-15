7ff55ddc3561ab34a554147fb0d593e0ce02f54e
kanhaji <myself.kanhaji@gmail.com>
2024-11-15 15:31:08 +0530

    feat: add Header component to AppLayout

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -2,9 +2,19 @@ type AppLayoutProps = {
   children: React.ReactNode;
 };
 
+const Header = () => {
+  return (
+    <div className="flex justify-between items-center">
+      <h1 className="text-2xl font-bold">Expense Tracer</h1>
+      <span className="text-2xl cursor-pointer">⚙️</span>
+    </div>
+  );
+};
+
 const AppLayout = ({ children }: AppLayoutProps) => {
   return (
-    <div>
+    <div className="mx-auto max-w-[625px] mt-2">
+      <Header />
       <div>{children}</div>
     </div>
   );
