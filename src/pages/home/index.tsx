b0044065b0b7e98fa3eca5e6a21c6b860da5a98e
kanhaji <myself.kanhaji@gmail.com>
2024-11-26 19:54:32 +0530

    feat: add progress indicator for daily expenses with warning and danger thresholds

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,18 @@
 import { useState } from "react";
 import AppLayout from "../../layout";
+import { Progress } from "@/components/ui/progress";
+import { cn } from "@/lib/utils";
 
 const DAILY_LIMIT = 120;
+const WARNING_LIMIT = 100;
+const DANGER_LIMIT = 120;
 const CURRENCY = "₹";
+
+const percentage = (current: number, total: number) => {
+  if (current > total) return 100;
+
+  return (current / total) * 100;
+};
 
 const Home = () => {
   const [currentExpense, setCurrentExpense] = useState<number>(0);
@@ -27,6 +37,15 @@ const Home = () => {
           You have spent {CURRENCY} {currentExpense} today out of {CURRENCY}{" "}
           {DAILY_LIMIT}
         </h1>
+        <Progress
+          value={percentage(currentExpense, DAILY_LIMIT)}
+          className={cn({
+            "[&>div]:bg-red-500": currentExpense >= DANGER_LIMIT,
+            "[&>div]:bg-yellow-500":
+              currentExpense >= WARNING_LIMIT && currentExpense < DANGER_LIMIT,
+            "[&>div]:bg-green-500": currentExpense <= WARNING_LIMIT,
+          })}
+        />
 
         <form className="flex flex-col items-center" onSubmit={onSubmit}>
           <input
